bed0ec431395f42201226724196f8de3f7bdafcb
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-16 16:51:15 +0100

    added column in migration, starting seeder

--- database/migrations/2024_02_16_152434_create_comics_table.php
+++ database/migrations/2024_02_16_152434_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('thumb', 255);
+            $table->string('price', 255);
+            $table->string('series', 255);
+            $table->date('sale_date');
+            $table->string('type', 255);
+            $table->json('artist');
+            $table->json('writers');
             $table->timestamps();
         });
     }
--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -14,6 +15,10 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i=0; $i<10 ; $i++) { 
+            $comic = new Comic();
+
+            
+        }
     }
 }
