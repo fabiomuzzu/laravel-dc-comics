31c05fb26d1ef80d19d7c0b8de8459e8bb8b1426
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-16 17:01:41 +0100

    added infos in seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -15,10 +15,20 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=0; $i<10 ; $i++) { 
-            $comic = new Comic();
+        $comics = config('comics');
 
-            
+        foreach ($comics as $comic) {
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->artist = json_encode($comic['artists']);
+            $newComic->writers = json_encode($comic['writers']);
+            $newComic->save();
         }
     }
 }
